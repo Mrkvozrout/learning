c04970435dd495e9f7ade72d849540d55f23cdad
Jan Krupička <honza.krupicka@email.cz>
2023-03-31 15:26:43 +0200

    Udemy - The complete web development bootcamp - 20 security - passport google

--- Udemy/the-complete-web-development-bootcamp/20-security/app.js
+++ Udemy/the-complete-web-development-bootcamp/20-security/app.js
@@ -1,31 +1,30 @@
+const appConfig = require('./modules/config.js')
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
-const appConfig = require('./modules/config.js')
 const mongoose = require('mongoose')
-const session = require('express-session')
+const findOrCreate = require('mongoose-findorcreate')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 
 // Setup DB
 
 const userSchema = new mongoose.Schema({
-  // These should be created by passport based on the config options bellow
-  // email: {
-  //   type: String,
-  //   require: true,
-  //   unique: true
-  // },
-  // password: {
-  //   type: String,
-  //   require: true
-  // }
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  googleProfileId: String
 })
 userSchema.plugin(passportLocalMongoose, {
   usernameField: 'email',
   usernameUnique: true
 })
+userSchema.plugin(findOrCreate)
 
 const User = mongoose.model('User', userSchema)
 
@@ -36,8 +35,35 @@ mongoose.connect(appConfig.mongoConnectionUri)
 // Setup passport (auth)
 
 passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+
+passport.serializeUser(function(user, cb) {
+  return cb(null, {
+    id: user.id,
+    username: user.username,
+    picture: user.picture
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  return cb(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: appConfig.googleClientId,
+    clientSecret: appConfig.googleClientSecret,
+    callbackURL: 'http://localhost:3000/auth/google/secrets'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    User.findOrCreate({
+      googleProfileId: profile.id,
+      email: profile._json.email
+    },
+    (err, user) => {
+      return cb(err, user);
+    });
+  }
+));
 
 
 // Setup app
@@ -93,7 +119,7 @@ app.route('/login')
 
     req.login(user, function(err) {
       if (err) {
-        console.log('Failed to login: ' + err);
+        console.log('Failed to login: ' + err)
         res.redirect('/login')
         return
       }
@@ -109,10 +135,10 @@ app.route('/logout')
   .get((req, res) => {
     req.logout((err) => {
       if (err) {
-        console.log('Failed to logout: ' + err);
+        console.log('Failed to logout: ' + err)
         return
       }
-      
+
       res.redirect('/')
     })
   })
@@ -144,6 +170,17 @@ app.route('/register')
       })
     })
   })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
+  }
+);
 
 app.route('/secrets')
 
--- Udemy/the-complete-web-development-bootcamp/20-security/modules/config.js
+++ Udemy/the-complete-web-development-bootcamp/20-security/modules/config.js
@@ -7,5 +7,9 @@ module.exports = {
   mongoPassword: process.env.MONGO_PASSWORD,
   encryptionSecret: process.env.ENC_SEC,
   sessionSecret: process.env.SESSION_SEC,
-  env: process.env.NODE_ENV || 'production'
+  env: process.env.NODE_ENV || 'production',
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientId: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
 }
